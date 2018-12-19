52acf50e132d9fcf032f6305846827d771b9bdb9
Nedim Sisic <nsisic1@etf.unsa.ba>
2018-12-19 23:32:26 +0100

    Dodana pomocna metoda generirajTabeleAkoNePostoje

--- src/ba/unsa/etf/rpr/GeografijaDAO.java
+++ src/ba/unsa/etf/rpr/GeografijaDAO.java
@@ -1,21 +1,61 @@
 package ba.unsa.etf.rpr;
 
+import java.sql.*;
+
 public class GeografijaDAO {
 
     private static GeografijaDAO instance = null;
 
-    private static void initialize() { // TODO: zabiljezi: staticna metoda, kreira intancu
+    private Connection conn;
+
+    private static void initialize() throws SQLException { // TODO: zabiljezi: staticna metoda, kreira intancu
         instance = new GeografijaDAO();
     }
 
-    private GeografijaDAO() {
-
+    private GeografijaDAO() throws SQLException {
+        String url = "jdbc:sqlite:baza.db";
+        conn = DriverManager.getConnection(url);
+        try {
+            generirajTabeleAkoNePostoje();
+        } catch (SQLException e) {
+            // tabele baze vec postoje
+        }
     }
 
     public static GeografijaDAO getInstance() {
-        if (instance == null) {
-            initialize();
+        try {
+            if (instance == null) {
+                initialize();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return instance;
     }
+
+    private void generirajTabeleAkoNePostoje() throws SQLException { // TODO: kad se koji bacaju izuzetci
+        Statement stmt = conn.createStatement();
+        String generirajDrzave = "CREATE TABLE \"drzava\" ( `id` INTEGER, `naziv` TEXT, `glavni_grad` INTEGER," +
+                "FOREIGN KEY(`glavni_grad`) REFERENCES `grad`, PRIMARY KEY(`id`) )";
+        try {
+            stmt.executeQuery(generirajDrzave);
+        } catch (SQLException e) {
+            // Drzave vec postoje
+        }
+        String generirajGradove = "CREATE TABLE \"grad\" ( `id` INTEGER, `naziv` TEXT, `broj_stanovnika` INTEGER," +
+                " `drzava` INTEGER, PRIMARY KEY(`id`), FOREIGN KEY(`drzava`) REFERENCES `drzava` )";
+        try {
+            stmt.executeQuery(generirajGradove);
+        } catch (SQLException e) {
+            // Gradovi vec postoje
+        }
+
+        popuniTabele();
+    }
+
+    private void popuniTabele() throws SQLException {
+        //Statement stmt = conn.createStatement();
+
+    }
+
 }
--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -8,8 +8,11 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("Gradovi su:\n" + ispisiGradove());
         //glavniGrad();
+        GeografijaDAO geo = GeografijaDAO.getInstance();
+        System.out.println("Tntntnnt");
     }
 }
 
+// morao sam prebaciti (smanjiti) verziju jave na 8
 
 // https://stackoverflow.com/questions/52960935/android-studio-database-file-loaded-in-the-wrong-encoding-utf-8
