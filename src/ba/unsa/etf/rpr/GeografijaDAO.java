9e2e41b20ebea5436c972b5902e985bfa94d1e53
Nedim Sisic <nsisic1@etf.unsa.ba>
2018-12-19 20:49:24 +0100

    GeografijaDAO prosirena u Singleton

--- src/ba/unsa/etf/rpr/GeografijaDAO.java
+++ src/ba/unsa/etf/rpr/GeografijaDAO.java
@@ -1,4 +1,21 @@
 package ba.unsa.etf.rpr;
 
 public class GeografijaDAO {
+
+    private static GeografijaDAO instance = null;
+
+    private static void initialize() { // TODO: zabiljezi: staticna metoda, kreira intancu
+        instance = new GeografijaDAO();
+    }
+
+    private GeografijaDAO() {
+
+    }
+
+    public static GeografijaDAO getInstance() {
+        if (instance == null) {
+            initialize();
+        }
+        return instance;
+    }
 }
